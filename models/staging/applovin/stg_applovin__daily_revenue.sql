c57bdfba62dcb2f11b424c2a7d83aa0f425e0edd
zulqarnainhaider-corp <zulqarnain.haider@terafort.com>
2026-05-04 16:36:15 +0500

    Update stg_applovin__daily_revenue.sql

--- models/staging/applovin/stg_applovin__daily_revenue.sql
+++ models/staging/applovin/stg_applovin__daily_revenue.sql
@@ -1,1 +1,8 @@
+-- Staging model for AppLovin daily revenue
+-- Light wrapper over raw data; just selects all columns from the source.
+-- Materialized as a view (no storage cost).
 
+{{ config(materialized='view') }}
+
+SELECT *
+FROM {{ source('applovin_data', 'applovin_daily_revenue') }}
